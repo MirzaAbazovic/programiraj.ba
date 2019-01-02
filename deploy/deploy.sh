3569875504877f62b12e22107ae57a981e7506b2
Mirza Abazovic <mirza.abazovic@gmail.com>
2019-01-02 13:54:57 +0100

    lftp upload script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ls -la
 echo "Upload all files from blog/public to blog.programiraj.ba using lftp"
-lftp -e "set ftp:ssl-allow no; mirror -R ../blog/public /blog.programiraj.ba/; quit" -u $FTP_USERNAME,$FTP_PASSWORD ftp://programiraj.ba
+lftp -e "set ftp:ssl-allow no; mirror -R ~/project/blog/public /blog.programiraj.ba/; quit" -u $FTP_USERNAME,$FTP_PASSWORD ftp://programiraj.ba
 
